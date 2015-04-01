cf654af4c6162e44c6149aa104e9e6c2e8a3fbf2
Jan Schulte <hello@unexpected-co.de>
2015-04-01 15:58:33 +0200

    add test cases (not passing yet)

--- test/pianolang_test.exs
+++ test/pianolang_test.exs
@@ -1,7 +1,19 @@
 defmodule PianolangTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "process addition" do
+    assert Pianolang.exec("(+ 1 2)") == 3
+  end
+
+  test "process subtraction" do
+    assert Pianolang.exec("(- 5 2)") == 3
+  end
+
+  test "process multiplication" do
+    assert Pianolang.exec("(* 3 4)") == 12
+  end
+
+  test "process division" do
+    assert Pianolang.exec("(/ 14 2)") == 7
   end
 end
